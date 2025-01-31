91c8f6adbc2e4e91c702c7e37e8c379e651655c9
madhavan8610331381@gmail.com <madhavan8610331381@gmail.com>
2025-01-31 15:47:55 +0530

    messsage

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/Main_Blog")
+  .connect("mongodb+srv://madhavan:RMVeGsYUWABB6pUK@cluster0.a0ssi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB:", err));
 
